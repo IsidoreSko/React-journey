e73b5d95a3360aeab2c488c24660bfb302a38ab1
IsidoreSKO <sparagmos@hotmail.fr>
2024-09-02 16:12:04 +0200

    end

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
 import { Header } from "./src/Header";
+import { Item } from "./src/Item";
+import { MenuFilters } from "./src/MenuFilter";
+import { REACT_CARDS } from "./src/react-cards";
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const filters = [...new Set(REACT_CARDS.map((card) => card.category))];
+
   return (
     <main className="m-auto flex h-full max-w-4xl flex-col px-4">
       <Header />
+      <div className="mb-4 mt-8 flex flex-1 gap-4 overflow-auto max-lg:flex-col">
+        <MenuFilters filters={filters} currentFilter={searchParams.filter} />
+        <div className="size-full overflow-auto">
+          <div className="grid h-fit w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {REACT_CARDS.filter((card) => {
+              if (!searchParams.filter) return true;
+              return card.category === searchParams.filter;
+            }).map((card) => (
+              <Item
+                hideCategory={Boolean(searchParams.filter)}
+                key={card.name}
+                item={card}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- app/src/Header.jsx
+++ app/src/Header.jsx
@@ -1,4 +1,6 @@
 import { ReactSvg } from "./ReactSvg";
+import { Button } from "./Button";
+import { ShoppingBasket, User } from "lucide-react";
 
 export const Header = () => {
   return (
@@ -6,6 +8,14 @@ export const Header = () => {
       <h1 className="inlin-flex items-center gp-2 text-lg font-bold"></h1>
       <ReactSvg size={32} />
       <span>ReactJourney</span>
+      <div className="flex intems-center ml-auto gap-2">
+        <Button variant="ghost">
+          <ShoppingBasket size={24} />
+        </Button>
+        <Button variant="ghost">
+          <User size={24} />
+        </Button>
+      </div>
     </header>
   );
 };
